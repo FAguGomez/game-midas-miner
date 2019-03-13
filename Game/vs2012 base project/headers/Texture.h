7c2d9d0a93f9ce595f3c56d08212d7a98ff9cd85
Agustín Gómez <fagugomez1997@gmail.com>
2019-03-13 04:08:17 -0300

    Gem class final implementation

--- Game/vs2012 base project/headers/Texture.h
+++ Game/vs2012 base project/headers/Texture.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <Sdl.h>
+#include <SDL.h>
 #include <SDL_image.h>
 #include "Log_handler.h"
 
@@ -16,8 +16,8 @@ public:
 	void Render();
 
 protected:
-	SDL_Renderer *Renderer;
-	SDL_Texture *Texture;
-	SDL_Rect Clip;
-	SDL_Rect Pos;
+	SDL_Renderer* _mRenderer;
+	SDL_Texture* _mTexture;
+	SDL_Rect _mClip;
+	SDL_Rect _mPos;
 };
